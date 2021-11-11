fe6a1e8d53797ae95cc298f82d4e1bda49a3ceb5
aksentit <90751266+aksentit@users.noreply.github.com>
2021-11-11 10:16:54 +0300

    Add files via upload

---
+++ unitTest/password-main/ClassLibraryPassword/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ClassLibraryPassword")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibraryPassword")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// компонента COM, задайте для атрибута ComVisible этого типа значение TRUE.
+[assembly: ComVisible(false)]
+
+// Следующий идентификатор GUID представляет идентификатор typelib, если этот проект доступен через COM
+[assembly: Guid("abdf3fac-20fb-459e-9edc-0195d9bfdc74")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ unitTest/password-main/ClassLibraryPassword/UserControl1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibraryPassword
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+
+        public class PasswordChecker
+        {
+            public static bool validatePassword(string password)
+            {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+
+            if (!password.Any(Char.IsLower))
+                return false;
+
+            if (!password.Any(Char.IsUpper))
+                return false;
+
+            if (!password.Any(Char.IsDigit))
+                return false;
+
+            if (password.Intersect("!@#$%^&*()").Count() == 0)
+                return false;
+
+
+            return true;
+            }
+        
+
+        }
+
+}
